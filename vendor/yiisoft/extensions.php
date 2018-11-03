819d11d2ee893d2c570d0763a4c6b4a1c41cc31f
Muhammad Irfan Ardiansyah <irfan.ardiansyah@dwp.co.id>
2018-11-03 11:23:39 +0700

    add vendor

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,181 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.1.0.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.8.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/src',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.13.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.4.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'rmrevin/yii2-fontawesome' => 
+  array (
+    'name' => 'rmrevin/yii2-fontawesome',
+    'version' => '2.17.1.0',
+    'alias' => 
+    array (
+      '@rmrevin/yii/fontawesome' => $vendorDir . '/rmrevin/yii2-fontawesome',
+    ),
+  ),
+  'cebe/yii2-gravatar' => 
+  array (
+    'name' => 'cebe/yii2-gravatar',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@cebe/gravatar' => $vendorDir . '/cebe/yii2-gravatar/cebe/gravatar',
+    ),
+  ),
+  'dmstr/yii2-adminlte-asset' => 
+  array (
+    'name' => 'dmstr/yii2-adminlte-asset',
+    'version' => '2.6.2.0',
+    'alias' => 
+    array (
+      '@dmstr' => $vendorDir . '/dmstr/yii2-adminlte-asset',
+    ),
+  ),
+  'dmstr/yii2-db' => 
+  array (
+    'name' => 'dmstr/yii2-db',
+    'version' => '0.10.1.0',
+    'alias' => 
+    array (
+      '@dmstr/db' => $vendorDir . '/dmstr/yii2-db/db',
+      '@dmstr/db/tests' => $vendorDir . '/dmstr/yii2-db/db/tests',
+      '@dmstr/console' => $vendorDir . '/dmstr/yii2-db/console',
+    ),
+  ),
+  'pheme/yii2-toggle-column' => 
+  array (
+    'name' => 'pheme/yii2-toggle-column',
+    'version' => '0.7.0.0',
+    'alias' => 
+    array (
+      '@pheme/grid' => $vendorDir . '/pheme/yii2-toggle-column',
+    ),
+  ),
+  'pheme/yii2-settings' => 
+  array (
+    'name' => 'pheme/yii2-settings',
+    'version' => '0.5.0.0',
+    'alias' => 
+    array (
+      '@pheme/settings' => $vendorDir . '/pheme/yii2-settings',
+    ),
+  ),
+  'dmstr/yii2-helpers' => 
+  array (
+    'name' => 'dmstr/yii2-helpers',
+    'version' => '0.4.13.0',
+    'alias' => 
+    array (
+      '@dmstr/helpers' => $vendorDir . '/dmstr/yii2-helpers/src',
+    ),
+  ),
+  'dmstr/yii2-bootstrap' => 
+  array (
+    'name' => 'dmstr/yii2-bootstrap',
+    'version' => '0.2.1.0',
+    'alias' => 
+    array (
+      '@dmstr/bootstrap' => $vendorDir . '/dmstr/yii2-bootstrap',
+    ),
+  ),
+  'schmunk42/yii2-giiant' => 
+  array (
+    'name' => 'schmunk42/yii2-giiant',
+    'version' => '0.10.7.0',
+    'alias' => 
+    array (
+      '@schmunk42/giiant' => $vendorDir . '/schmunk42/yii2-giiant/src',
+    ),
+    'bootstrap' => 'schmunk42\\giiant\\Bootstrap',
+  ),
+  'mohorev/yii2-upload-behavior' => 
+  array (
+    'name' => 'mohorev/yii2-upload-behavior',
+    'version' => '0.2.1.0',
+    'alias' => 
+    array (
+      '@mohorev/file' => $vendorDir . '/mohorev/yii2-upload-behavior/src',
+    ),
+  ),
+  'yii-dream-team/yii2-upload-behavior' => 
+  array (
+    'name' => 'yii-dream-team/yii2-upload-behavior',
+    'version' => '1.2.3.0',
+    'alias' => 
+    array (
+      '@yiidreamteam/upload' => $vendorDir . '/yii-dream-team/yii2-upload-behavior/src',
+    ),
+  ),
+  'lesha724/yii2-math-captcha' => 
+  array (
+    'name' => 'lesha724/yii2-math-captcha',
+    'version' => '0.1.2.0',
+    'alias' => 
+    array (
+      '@lesha724/MathCaptcha' => $vendorDir . '/lesha724/yii2-math-captcha',
+    ),
+  ),
+  'yiidoc/yii2-redactor' => 
+  array (
+    'name' => 'yiidoc/yii2-redactor',
+    'version' => '2.0.1.0',
+    'alias' => 
+    array (
+      '@yii/redactor' => '/',
+    ),
+  ),
+  'akiraz2/yii2-blog' => 
+  array (
+    'name' => 'akiraz2/yii2-blog',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@akiraz2/blog' => $vendorDir . '/akiraz2/yii2-blog',
+    ),
+    'bootstrap' => 'akiraz2\\blog\\Bootstrap',
+  ),
+);
